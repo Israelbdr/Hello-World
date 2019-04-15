130142300a8f656df8984053b43a7cc8205ef186
Israel Reis <israelbdr@icloud.com>
2019-04-15 01:00:50 -0300

    Updating all files to final release

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("TESTE.html")
 
-@app.route('/<int:id>', methods=['GET', 'POST'])
-def buscJSON():
     if request.method == 'POST':
-        post = request.form['number']
-        response = requests.get("https://jsonplaceholder.typicode.com/posts"+post)
-        content = json.loads(response.content)
+        post = str(request.form['number'])
+        response = requests.get("https://jsonplaceholder.typicode.com/posts/" + post)
+        content = response.json()
         return render_template("TESTE2.html", content=content)
-    else:
-        return render_template('TESTE2.html')
+
+    return render_template("TESTE.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
