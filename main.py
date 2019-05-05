af64531b2221d58d4d6d420775a79333f35e46c9
Israel Reis <israelbdr@icloud.com>
2019-05-05 16:04:04 -0300

    Updating files to make a post request

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ app = Flask(__name__)
 def index():
 
     if request.method == 'POST':
-        post = str(request.form['number'])
-        response = requests.get("https://jsonplaceholder.typicode.com/posts/" + post)
-        content = response.json()
-        return render_template("TESTE2.html", content=content)
+        post = (request.form['number'])
 
+        if (request.form['userId'] == ''):
+            response = requests.get("https://jsonplaceholder.typicode.com/posts/" + post)
+            content = response.json()
+            return render_template("TESTE2.html", content=content)
+        else:
+            title = request.form['title']
+            body = request.form['body']
+            userid = request.form['userId']
+            data = {'title':title, 'body':body, 'userId':userid}
+            response = requests.post("https://jsonplaceholder.typicode.com/posts/", data)
+            content = response.json()
+            return render_template("TESTE2.html", content=content)
     return render_template("TESTE.html")
 
 if __name__ == "__main__":
